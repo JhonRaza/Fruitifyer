b49ef9d7380dac457dbf6d9a232324f54c9cd30b
JhonRaza <45493553+JhonRaza@users.noreply.github.com>
2022-10-07 01:08:52 +0500

    Update firebase.js

--- database/firebase.js
+++ database/firebase.js
@@ -5,14 +5,7 @@ import 'firebase/compat/firestore';
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDPJsDJwdOnfzaWWhxxEAJkPcllqTkcqv8",
-    authDomain: "fruitifyer-ca6fa.firebaseapp.com",
-    databaseURL: "https://fruitifyer-ca6fa-default-rtdb.firebaseio.com",
-    projectId: "fruitifyer-ca6fa",
-    storageBucket: "fruitifyer-ca6fa.appspot.com",
-    messagingSenderId: "715440869732",
-    appId: "1:715440869732:web:d90dd5a91c9dea00572a71",
-    measurementId: "G-P6RM5TJVJ0"
+    //config here
 };
 
 firebase.initializeApp(firebaseConfig);
